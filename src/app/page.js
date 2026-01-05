330f4ef64e6d0913528e0d6c24ea3dfb32432b25
NigarHasanzda <nigarhesenzade703@gmail.com>
2026-01-05 11:25:26 +0400

    Hero components

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,38 @@
 import Image from "next/image";
+import "./globals.css";
 
 export default function Home() {
   return (
-    <div >
-hi
+<>
+  <section id="hero-section">
+    <div className="container">
+      <div className="hero-text">
+        <h1 className="hero-header">Make your interior more
+minimalistic & modern</h1>
+        <p className="hero-description">Turn your room with panto into a lot more minimalist and modern with ease and speed</p>
+        <input type="text" placeholder="Search for products..." className="search-bar" />
+      </div>
+
     </div>
+
+  </section>
+
+  <section id="four">
+    <div className="four-container">
+      <div className="four-item"> 
+        </div>
+      <div className="four-item"> 
+        </div>
+      <div className="four-item"> 
+        </div>
+      <div className="four-item"> 
+        </div>
+      <div className="four-item"> 
+        </div>
+
+    </div>
+    
+  </section>
+  </>
   );
 }
